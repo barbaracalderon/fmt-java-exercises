35548ee060f5a3fcf76396594ef9e3adc0cf9d68
barbaracalderon <bcalderoni.ti@gmail.com>
2024-02-05 16:52:47 -0300

    feat: create Curso class

--- src/Curso.java
+++ src/Curso.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class Curso {
+
+    private final String nome;
+    private String descricao;
+    private ArrayList<String> professoresList;
+    private int cargaHoraria;
+
+    public Curso(String nome, String descricao, ArrayList<String> professoresList, int cargaHoraria) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.professoresList = professoresList;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public void progresso() {
+        System.out.println("O curso está em progresso.");
+    }
+
+    public void notas() {
+        System.out.println("As notas deste curso ainda não foram lançadas.");
+    }
+
+    public void participacao() {
+        System.out.println("Alguns professores fazem fazem parte deste curso, ensinando.");
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> professoresList = new ArrayList<>();
+        Curso curso = new Curso("Física", "Disciplina de física mecânica e relativista.", professoresList, 20);
+        curso.progresso();
+        curso.notas();
+        curso.participacao();
+    }
+
 }
