b1edce24b934ff3774c4470623cd5d59225e3963
barbaracalderon <bcalderoni.ti@gmail.com>
2024-02-11 22:07:37 -0300

    feat: create class Jogo

---
+++ src/Jogo.java
@@ -0,0 +1,7 @@
+public class Jogo {
+    public static void main (String[] args) {
+        Jogador jogadorUm = new Jogador("Maria", 25, 0, 0);
+        Jogador jogadorDois = new Jogador("Pedro", 25, 0, 0);
+
+    }
+}
